c255fbc0799c09d2dd319d5157505b043d8eaf0c
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-10-14 15:19:10 +0200

    [FIX] fixed name of the route

--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/index', [ProjectController::class, 'home'])->name('project_home');
+Route::get('/index', [ProjectController::class, 'index'])->name('project_home');
